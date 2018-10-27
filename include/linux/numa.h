ae596c28a42ee3777d28db0984a0780d38eed11a
TOT0Ro <tot0roprog@gmail.com>
2018-10-28 06:55:15 +0900

    Analysis (#12)
    * 10/20 보충
    
    * 2018.10.27 Mike.C, 코딩의노예, 바뿌사, 코딩코딩

--- include/linux/numa.h
+++ include/linux/numa.h
@@ -2,13 +2,14 @@
 #ifndef _LINUX_NUMA_H
 #define _LINUX_NUMA_H
 
-
+// IMRT >> 2
 #ifdef CONFIG_NODES_SHIFT
 #define NODES_SHIFT     CONFIG_NODES_SHIFT
 #else
 #define NODES_SHIFT     0
 #endif
 
+// IMRT >> 1<<2 == 4
 #define MAX_NUMNODES    (1 << NODES_SHIFT)
 
 #define	NUMA_NO_NODE	(-1)
